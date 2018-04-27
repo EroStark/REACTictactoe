89f50bfd535178ed67fc8c03c8a843ef1384dd3f
erostark <eioncontaste@ac.c4q.nyc>
2018-04-27 11:45:51 -0400

    add click functionality

--- src/components/board.js
+++ src/components/board.js
@@ -3,10 +3,24 @@ import Square from './square'
 
 
 class Board extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null)
+        };
+    }
+    
+
+    
     renderSquare(i) {
-      return <Square value={i} />;
+      return (<Square value={this.state.squares[i]} onClick={() => this.handleclick(i)} />);
     }
   
+    handleclick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] ='X'
+        this.setState({squares: squares})
+    }
     render() {
       const status = 'Next player: X';
   
--- src/components/square.js
+++ src/components/square.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 class Square extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+        value: null
+        }
+    }
+    
     render() {
       return (
-        <button className="square" onClick={() => alert('deez nutz')}>
+        <button className="square" onClick={() => this.props.onClick()}>
           {this.props.value}
         </button>
       );
